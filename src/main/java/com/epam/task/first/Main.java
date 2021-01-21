a43b7025153f7652642879be8e378100c32d23b6
Eugene <y.vezhnavets@mail.ru>
2021-01-21 23:23:25 +0300

    added test

--- src/main/java/com/epam/task/first/Main.java
+++ src/main/java/com/epam/task/first/Main.java
@@ -1,7 +1,15 @@
 package com.epam.task.first;
 
+import com.epam.task.first.entities.Array;
+import com.epam.task.first.view.ArrayPrinter;
+import com.epam.task.first.view.ConsoleArrayPrinter;
+
 public class Main {
     public static void main(String[] args) {
 
+
+        Array array = new Array(1,2,3);
+        ArrayPrinter arrayPrinter = new ConsoleArrayPrinter();
+        arrayPrinter.print(array);
     }
 }
